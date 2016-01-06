27ebaa595f10be6c70227943db475108449b12d3
Stefan <git@stefan-sielaff.de>
2016-01-06 18:02:10 +0100

    Basic Skin

--- resources/lib/gui.py
+++ resources/lib/gui.py
@@ -3,6 +3,7 @@ import xbmc
 import xbmcgui
 import settings
 import utils
+import datastore
 
 __settings__ = settings.Settings()
 
@@ -13,6 +14,7 @@ KEY_KEYBOARD_ESC = 61467
 
 CONTROLID_MAIN_MENU = 9100
 CONTROLID_TOP_LABEL = 9201
+CONTROLID_TOP_LOADING = 9202
 
 
 class GUI(xbmcgui.WindowXMLDialog):
@@ -20,19 +22,21 @@ class GUI(xbmcgui.WindowXMLDialog):
     __main_menu = None
     __main_selected = None
 
-    def onInit(self):
+    __datastore = None
 
+    def __init__(self, xmlFilename, scriptPath, *args, **kwargs):
+        self.DASHBOARD = xbmcgui.ListItem(utils.lang(32500), "DASHBOARD", iconImage="icons/ic_cloud_download_white_48dp_2x.png")
+        self.ALBUMS = xbmcgui.ListItem(utils.lang(32501), "ALBUMS", iconImage="icons/ic_album_white_48dp_2x.png")
+        self.ARTISTS = xbmcgui.ListItem(utils.lang(32502), "ARTISTS", iconImage="icons/ic_people_outline_white_48dp_2x.png")
+        self.SETTINGS = xbmcgui.ListItem(utils.lang(32503), "SETTINGS", iconImage="icons/ic_settings_white_48dp_2x.png")
+        self.EXIT = xbmcgui.ListItem(utils.lang(32504), "EXIT", iconImage="icons/ic_power_settings_new_white_48dp_2x.png")
+        super(GUI, self).__init__(xmlFilename, scriptPath, *args, **kwargs)
+
+    def onInit(self):
         # Main Menu
         self.__main_menu = self.getControl(9100)
-        main_items = [
-            xbmcgui.ListItem(utils.lang(32500), "DASHBOARD", iconImage="icons/ic_cloud_download_white_48dp_2x.png"),
-            xbmcgui.ListItem(utils.lang(32501), "ALBUMS", iconImage="icons/ic_album_white_48dp_2x.png"),
-            xbmcgui.ListItem(utils.lang(32502), "ARTISTS", iconImage="icons/ic_people_outline_white_48dp_2x.png"),
-            xbmcgui.ListItem(utils.lang(32503), "SETTINGS", iconImage="icons/ic_settings_white_48dp_2x.png"),
-            xbmcgui.ListItem(utils.lang(32504), "EXIT", iconImage="icons/ic_power_settings_new_white_48dp_2x.png"),
-        ]
-        self.__main_menu.addItems(main_items)
-        self.setSelected(self.__main_menu.getSelectedItem())
+        self.__main_menu.addItems([self.DASHBOARD, self.ALBUMS, self.ARTISTS, self.SETTINGS, self.EXIT])
+        self.setMainSelected(self.__main_menu.getSelectedItem())
         self.setFocus(self.__main_menu)
 
     def onClick(self, control_id):
@@ -53,7 +57,7 @@ class GUI(xbmcgui.WindowXMLDialog):
             self.setFocusId(CONTROLID_MAIN_MENU)
 
         if self.__main_menu is not None:
-            self.setSelected(self.__main_menu.getSelectedItem())
+            self.setMainSelected(self.__main_menu.getSelectedItem())
 
         if self.__main_selected is not None:
             pass
@@ -61,10 +65,33 @@ class GUI(xbmcgui.WindowXMLDialog):
         if action.getButtonCode() in (KEY_BUTTON_BACK, KEY_KEYBOARD_ESC):
             self.exit()
 
-    def setSelected(self, control):
-        self.__main_selected = control
-        self.getControl(CONTROLID_TOP_LABEL).setLabel(self.__main_selected.getLabel())
+    def setMainSelected(self, control, force=False):
+        # handles selection in main menu, only if selection is changed
+        if self.__main_selected is None or force or self.__main_selected.getLabel2() != control.getLabel2():
+            self.__main_selected = control
+            self.getControl(CONTROLID_TOP_LABEL).setLabel(self.bold(self.__main_selected.getLabel()))
+            self.updateMainSelection()
+
+    def updateMainSelection(self, force=False):
+        self.getControl(CONTROLID_TOP_LOADING).setVisible(True)
+
+        selected = self.__main_selected.getLabel2()
+        if selected == self.DASHBOARD.getLabel2():
+            if self.__datastore is None or force:
+                self.__datastore = datastore.Datastore(self.dashboardCallback)
+
+        self.getControl(CONTROLID_TOP_LOADING).setVisible(False)
+
+    def dashboardCallback(self, index, albums_len, album_artist, album_title=None):
+        if album_title is None:
+            self.getControl(9302).setLabel(str(index))
+        else:
+            self.getControl(9301).setLabel(str(index))
 
     def exit(self):
         self.close()
         settings.log("Closing", xbmc.LOGNOTICE)
+
+    @staticmethod
+    def bold(txt):
+        return "[B]%s[/B]" % txt
